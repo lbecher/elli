0104c1019cfe57c19f404699649bd5a5b668b22d
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2022-05-29 06:44:01 -0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -11,7 +11,7 @@ pluks_uuid=$( blkid -o value -s UUID /dev/${pluks} )
 grub_p1=$( cat /root/elli/conf/grub_p1.conf )
 grub_p2=$( cat /root/elli/conf/grub_p2.conf )
 
-useradd -m -G wheel luiz
+useradd -m -G wheel $user
 
 echo "Set root password:"
 passwd
@@ -32,13 +32,22 @@ echo -e "127.0.0.1 localhost.localdomain localhost\n::1 localhost.localdomain lo
 echo "luks-$pluks_uuid UUID=$pluks_uuid none discard" > /etc/crypttab
 
 cp dracut-install.sh /usr/local/bin/
+cp dracut-remove.sh /usr/local/bin/
+chmod +x /usr/local/bin/dracut-install.sh
+chmod +x /usr/local/bin/dracut-remove.sh
+
+cp "90-dracut-install.hook" /etc/pacman.d/hooks/
+cp "60-dracut-remove.hook" /etc/pacman.d/hooks/
+ln -sf /dev/null /etc/pacman.d/hooks/90-mkinitcpio-install.hook
+ln -sf /dev/null /etc/pacman.d/hooks/60-mkinitcpio-remove.hook
+
 /usr/local/bin/dracut-install.sh
 
-#grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-#grub-mkconfig -o /boot/grub/grub.cfg
-#echo "${grub_p1}" > /etc/default/grub
-#echo "GRUB_CMDLINE_LINUX=\"rd.luks.uuid=luks-$pluks_uuid rhgb quiet\"" >> /etc/default/grub
-#echo "${grub_p2}" >> /etc/default/grub;
-#grub-mkconfig -o /boot/grub/grub.cfg;
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+echo "${grub_p1}" > /etc/default/grub
+echo "GRUB_CMDLINE_LINUX=\"rd.luks.uuid=luks-$pluks_uuid rhgb quiet\"" >> /etc/default/grub
+echo "${grub_p2}" >> /etc/default/grub;
+grub-mkconfig -o /boot/grub/grub.cfg;
 
 systemctl enable NetworkManager;
