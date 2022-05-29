f6cfba335aaa96bae36168e12db7626ff290b42f
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2022-05-28 22:37:06 -0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-gname=$( cat /root/elli/conf/gname.conf );
-hname=$( cat /root/elli/conf/hname.conf );
+gname="archlvm" #$( cat /root/elli/conf/gname.conf )
+hname="arch" #$( cat /root/elli/conf/hname.conf )
 
-pluks=$( cat /root/elli/conf/pluks.conf );
-pluks_uuid=$( blkid -o value -s UUID /dev/${pluks} );
+pluks="nvme0n1p1" #$( cat /root/elli/conf/pluks.conf )
+pluks_uuid=$( blkid -o value -s UUID /dev/${pluks} )
 
-grub_p1=$( cat /root/elli/conf/grub_p1.conf );
-grub_p2=$( cat /root/elli/conf/grub_p2.conf );
-mkinitcpio=$( cat /root/elli/conf/mkinitcpio.conf );
+grub_p1=$( cat /root/elli/conf/grub_p1.conf )
+grub_p2=$( cat /root/elli/conf/grub_p2.conf )
+#mkinitcpio=$( cat /root/elli/conf/mkinitcpio.conf )
 
-echo "Setup root password:";
-passwd;
+echo "Setup root password:"
+passwd
 
-echo -e "pt_BR.UTF-8 UTF-8\nen_US.UTF-8 UTF-8" > /etc/locale.gen;
-locale-gen;
-echo "LANG=pt_BR.UTF-8" > /etc/locale.conf;
-echo "KEYMAP=br-abnt2" > /etc/vconsole.conf;
-ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime;
-hwclock --systohc;
+echo -e "pt_BR.UTF-8 UTF-8\nen_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
+echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+hwclock --systohc
 
-echo "${hname}" > /etc/hostname;
-echo -e "127.0.0.1 localhost.localdomain localhost\n::1 localhost.localdomain localhost\n127.0.1.1 ${hname}.localdomain ${hname}" > /etc/hosts;
+echo "$hname" > /etc/hostname
+echo -e "127.0.0.1 localhost.localdomain localhost\n::1 localhost.localdomain localhost\n127.0.1.1 $hname.localdomain $hname" > /etc/hosts
 
-echo "${mkinitcpio}" > /etc/mkinitcpio.conf;
-mkinitcpio -P;
+#echo "${mkinitcpio}" > /etc/mkinitcpio.conf
+#mkinitcpio -P
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB;
-grub-mkconfig -o /boot/grub/grub.cfg;
-echo "${grub_p1}" > /etc/default/grub;
-echo "GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=${pluks_uuid}:${pluks_uuid}_crypt root=/dev/${gname}/root\"" >> /etc/default/grub
+echo "luks-$pluks_uuid UUID=$pluks_uuid none discard" > /etc/crypttab
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+echo "${grub_p1}" > /etc/default/grub
+echo "GRUB_CMDLINE_LINUX=\"rd.luks.uuid=luks-$pluks_uuid rhgb quiet\"" >> /etc/default/grub
 echo "${grub_p2}" >> /etc/default/grub;
 grub-mkconfig -o /boot/grub/grub.cfg;
 
