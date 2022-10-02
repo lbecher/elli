9331e608ac29efe591cc80fdfa2a70454d4e6019
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2022-10-02 11:24:22 -0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-gname="$2"
-hname="$3"
+gname="archlvm"
+hname="a515-54g"
 
-pluks="$1"
+pluks="/dev/nvme0n1p3"
 pluks_uuid=$( blkid -o value -s UUID ${pluks} )
+pluks_name="luks-$pluks_uuid"
+
+#proot="/dev/archlvm/root"
+#proot_uuid=$( blkid -o value -s UUID ${proot} )
 
 grub_p1=$( cat /root/elli/conf/grub_p1.conf )
 grub_p2=$( cat /root/elli/conf/grub_p2.conf )
@@ -19,28 +23,27 @@ hwclock --systohc
 echo "$hname" > /etc/hostname
 echo -e "127.0.0.1 localhost.localdomain localhost\n::1 localhost.localdomain localhost\n127.0.1.1 $hname.localdomain $hname" > /etc/hosts
 
-echo "luks-$pluks_uuid UUID=$pluks_uuid none discard" > /etc/crypttab
+echo "$pluks_name UUID=$pluks_uuid none discard" > /etc/crypttab
 
-cp dracut-install.sh /usr/local/bin/
-cp dracut-remove.sh /usr/local/bin/
-chmod +x /usr/local/bin/dracut-install.sh
-chmod +x /usr/local/bin/dracut-remove.sh
+#cp dracut-install.sh /usr/local/bin/
+#cp dracut-remove.sh /usr/local/bin/
+#chmod +x /usr/local/bin/dracut-install.sh
+#chmod +x /usr/local/bin/dracut-remove.sh
 
-mkdir /etc/pacman.d/hooks
-cp "90-dracut-install.hook" /etc/pacman.d/hooks/
-cp "60-dracut-remove.hook" /etc/pacman.d/hooks/
-ln -sf /dev/null /etc/pacman.d/hooks/90-mkinitcpio-install.hook
-ln -sf /dev/null /etc/pacman.d/hooks/60-mkinitcpio-remove.hook
+#mkdir /etc/pacman.d/hooks
+#cp "90-dracut-install.hook" /etc/pacman.d/hooks/
+#cp "60-dracut-remove.hook" /etc/pacman.d/hooks/
+#ln -sf /dev/null /etc/pacman.d/hooks/90-mkinitcpio-install.hook
+#ln -sf /dev/null /etc/pacman.d/hooks/60-mkinitcpio-remove.hook
+#pacman -Sy linux
 
-pacman -Sy linux
-
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=ArchLinux
 grub-mkconfig -o /boot/grub/grub.cfg
 echo "${grub_p1}" > /etc/default/grub
-echo "GRUB_CMDLINE_LINUX=\"rd.luks.uuid=luks-$pluks_uuid rhgb quiet\"" >> /etc/default/grub
+echo "GRUB_CMDLINE_LINUX=\"rd.luks.uuid=$pluks_name rhgb quiet\"" >> /etc/default/grub
 echo "${grub_p2}" >> /etc/default/grub;
 grub-mkconfig -o /boot/grub/grub.cfg;
 
 systemctl enable NetworkManager;
-
-pacman -Sy gdm gnome firefox vlc nautilus;
+systemctl enable cups.socket;
+systemctl enable bluetooth.service;
