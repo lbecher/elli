5662ce1a8249b5939c84e26f33aac090951e569e
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2022-10-02 10:33:04 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
 
-pboot="$1"
-pluks="$2"
+pefi="/dev/nvme0n1p1"
+pboot="/dev/nvme0n1p2"
+pluks="/dev/nvme0n1p3"
+
+mkfs.ext4 $pluks
+
 pluks_uuid=$( blkid -o value -s UUID $pluks )
-proot_size="$3"
-gname="$4"
+plvm="/dev/mapper/luks-$pluks_uuid"
+plvm_name="luks-$pluks_uuid"
+
+proot_size="24GB"
+
+gname="archlvm"
 
 cryptsetup -s 256 -h sha256 -c aes-xts-plain64 luksFormat $pluks
-cryptsetup luksOpen $pluks luks-$pluks_uuid
+cryptsetup luksOpen $pluks $plvm_name
 
-pvcreate /dev/mapper/luks-$pluks_uuid
-vgcreate $gname /dev/mapper/luks-$pluks_uuid
+pvcreate $plvm
+vgcreate $gname $plvm
 lvcreate -C n -L $proot_size -n root $gname
 lvcreate -C n -l 100%FREE -n home $gname
 
-mkfs.vfat -F32 $pboot
+mkfs.vfat -F32 $pefi
+mkfs.ext4 $pboot
 mkfs.ext4 /dev/$gname/root
 mkfs.ext4 /dev/$gname/home
 
 mount /dev/$gname/root /mnt
 mkdir /mnt/home
 mkdir /mnt/boot
+mkdir /mnt/efi
 mount /dev/$gname/home /mnt/home
 mount $pboot /mnt/boot
+mount $pefi /mnt/efi
 
-extra="intel-ucode amd-ucode power-profiles-daemon mesa vulkan-intel intel-media-driver pipewire pipewire-alsa pipewire-pulse pipewire-jack wayland avahi libcups cups cups-pdf dbus-python gobject-introspection-runtime python-cairo python-gobject python-pycups python-pycurl ghostscript foomatic-db-gutenprint-ppds foomatic-db-engine sane sane-airscan ipp-usb nano ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family cantarell-fonts ttf-opensans ttf-croscore bluez bluez-utils ffmpeg gst-libav gstreamer-vaapi"
+fonts="ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family cantarell-fonts ttf-opensans ttf-croscore"
+midia="mesa wayland pipewire pipewire-alsa pipewire-pulse pipewire-jack gstreamer gstreamer-vaapi gst-plugin-pipewire ffmpeg libva-mesa-driver"
+printer="avahi cups cups-pdf libcups ghostscript gutenprint foomatic-db-engine foomatic-db foomatic-db-ppds foomatic-db-nonfree foomatic-db-nonfree-ppds foomatic-db-gutenprint-ppds"
+bluetooth="bluez bluez-utils"
+cpu="amd-ucode power-profiles-daemon"
+base="base base-devel linux linux-headers linux-firmware grub efibootmgr dbus lvm2 cryptsetup networkmanager git nano"
 
-pacstrap /mnt base base-devel linux linux-headers linux-firmware grub efibootmgr dracut dbus lvm2 cryptsetup networkmanager python3 git nano $extra;
+pacstrap /mnt $fonts $midia $printer $bluetooth $cpu $base;
 
 genfstab -U /mnt > /mnt/etc/fstab
-
-cp -r ./elli /mnt/root
