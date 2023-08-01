f1626a77f35bb804b0d6aae203e75d48c6325d92
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2023-08-01 12:17:01 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# Configure
+
 gname="archlvm"
 
 pefi="/dev/nvme0n1p1"
 pboot="/dev/nvme0n1p2"
 pluks="/dev/nvme0n1p3"
 
-mkfs.ext4 $pluks
-pluks_uuid=$( blkid -o value -s UUID $pluks )
+use_swap="y"
+use_home="y"
+
+# Install
+
+cryptsetup -s 256 -h sha256 -c aes-xts-plain64 luksFormat "$pluks"
+
+pluks_uuid=$( blkid -o value -s UUID "$pluks" )
 plvm="/dev/mapper/luks-$pluks_uuid"
 plvm_name="luks-$pluks_uuid"
 
@@ -17,31 +25,52 @@ echo "${mirrorlist}" > /etc/pacman.d/mirrorlist
 
 nano /etc/pacman.conf
 
-cryptsetup -s 256 -h sha256 -c aes-xts-plain64 luksFormat $pluks
-cryptsetup luksOpen $pluks $plvm_name
+cryptsetup luksOpen "$pluks" "$plvm_name"
 
-pvcreate $plvm
-vgcreate $gname $plvm
+pvcreate "$plvm"
+vgcreate "$gname" "$plvm"
 
-mkfs.vfat -F32 $pefi
-mkfs.ext4 $pboot
+mkfs.vfat -F32 "$pefi"
+mkfs.xfs "$pboot"
 
-lvcreate -C y -L 8GB -n swap $gname
-mkswap /dev/$gname/swap
+if [ "$use_swap" = "y" ]; then
+  lvcreate -C y -L 16GB -n swap $gname
+  mkswap /dev/$gname/swap
+  swapon /dev/$gname/swap
+fi
 
-lvcreate -C n -l 100%FREE -n root $gname
-mkfs.ext4 /dev/$gname/root
+if [ "$use_home" = "y" ]; then
+  lvcreate -C n -L 64GB -n root "$gname"
+  lvcreate -C n -l 100%FREE -n home "$gname"
+  
+  mkfs.xfs "/dev/$gname/root"
+  mkfs.xfs "/dev/$gname/home"
 
-mount /dev/$gname/root /mnt
+  mount "/dev/$gname/root" "/mnt"
 
-mkdir /mnt/boot
-mkdir /mnt/efi
+  mkdir -p "/mnt/efi"
+  mkdir -p "/mnt/boot"
+  mkdir -p "/mnt/home"
 
-mount $pboot /mnt/boot
-mount $pefi /mnt/efi
+  mount "$pefi" "/mnt/efi"
+  mount "$pboot" "/mnt/boot"
+  mount "/dev/$gname/home" "/mnt/home"
+else
+  lvcreate -C n -l 100%FREE -n root "$gname"
+  mkfs.xfs "/dev/$gname/root"
+  mount "/dev/$gname/root" "/mnt"
 
-swapon /dev/$gname/swap
+  mkdir -p "/mnt/efi"
+  mkdir -p "/mnt/boot"
 
-pacstrap /mnt base base-devel linux linux-headers linux-firmware grub efibootmgr lvm2 cryptsetup intel-ucode amd-ucode git nano fuse rust python3 python-pip gcc clang make
+  mount "$pefi" "/mnt/efi"
+  mount "$pboot" "/mnt/boot"
+fi
+
+pacstrap /mnt base base-devel \
+  linux linux-headers linux-firmware \
+  intel-ucode amd-ucode \
+  grub efibootmgr lvm2 cryptsetup xfsprogs \
+  networkmanager git nano
 
 genfstab -U /mnt > /mnt/etc/fstab
